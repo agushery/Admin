eb930c88bd896a0e949adaa330e8dab60a764a74
agushery <hebatmandor@gmail.com>
2021-05-01 10:27:25 +0800

    update alert bootstrap 5

--- app/core/Flasher.php
+++ app/core/Flasher.php
@@ -13,12 +13,19 @@ class Flasher
     public static function flash()
     {
         if (isset($_SESSION['flash'])) {
-            echo '<div class="alert alert-'. $_SESSION['flash']['type'] . ' alert-dismissible fade show" role="alert">
-                    Data Citizen<strong>'. $_SESSION['flash']['message'] .'</strong> '. $_SESSION['flash']['action'] .'
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>';
+            // echo '<div class="alert alert-' . $_SESSION['flash']['type'] . ' alert-dismissible fade show" role="alert">
+            //         Data Citizen<strong>' . $_SESSION['flash']['message'] . '</strong> ' . $_SESSION['flash']['action'] . '
+            //         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            //             <span aria-hidden="true">&times;</span>
+            //         </button>
+            //     </div>';
+            // unset($_SESSION['flash']);
+            echo '<div class="alert alert-'. $_SESSION['flash']['type'] .' alert-dismissible fade show" role="alert">
+                Data Citizen
+                <strong> '. $_SESSION['flash']['message'] .'</strong> '. $_SESSION['flash']['action'] .' 
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"><button>
+            </div>
+            ';
             unset($_SESSION['flash']);
         }
     }
